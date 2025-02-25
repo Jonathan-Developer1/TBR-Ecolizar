c7c81a431c3c3b6e7125db2d241fa80f91792433
Jonathan <jonathanrubens10@gmail.com>
2025-02-25 02:03:08 -0300

    Sistema de login

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAqYjpVivu5IweqAlgakgt6m_ol2CQrLDM",
+  authDomain: "todolist-38d5d.firebaseapp.com",
+  projectId: "todolist-38d5d",
+  storageBucket: "todolist-38d5d.appspot.com",
+  messagingSenderId: "991802131508",
+  appId: "1:991802131508:web:f01c57ad15963a8cf03661"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
